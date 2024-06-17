fc9f390d352053540949c1942cc485a5af06769f
abrehan2 <rehannajam2@gmail.com>
2024-06-17 16:22:53 +0500

    Chore: Integrating next-auth

--- actions/register.ts
+++ actions/register.ts
@@ -3,6 +3,9 @@
 // IMPORTS -
 import * as z from "zod";
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validateFields = RegisterSchema.safeParse(values);
@@ -10,6 +13,26 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
   if (!validateFields.success) {
     return { error: "Invalid fields" };
   }
+
+  const { email, name, password } = validateFields.data;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "User already exists" };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send email verification
 
   return { success: "Email sent" };
 };
---
+++ data/user.ts
@@ -0,0 +1,32 @@
+// IMPORTS -
+import { db } from "@/lib/db";
+
+// GET USER BY EMAIL -
+export const getUserByEmail = async (email: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  } catch {
+    return null;
+  }
+};
+
+// GET USER BY ID -
+export const getUserByID = async (id: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  } catch {
+    return null;
+  }
+};
